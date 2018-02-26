5022604693c63254b328ed71357a7c4503a89866
Brandon <36675220+ImChurch@users.noreply.github.com>
2018-02-26 18:14:50 -0500

    Add files via upload

---
+++ SavingsAccount.java
@@ -0,0 +1,15 @@
+//methods and fields defined in this class are exclusively for objects of this class.
+public class SavingsAccount extends Bankaccount {
+	public SavingsAccount(double rate) {
+		intrestRate = rate;
+		
+	}
+	public void addIntrest() {
+		double intrest = getBalance(1) * intrestRate / 100;
+		deposit(intrest);
+		
+	}
+	
+	private double intrestRate;
+	
+}
